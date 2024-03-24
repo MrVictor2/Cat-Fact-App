9fb1ec74b5d68e45e2685c7f3a2fdc372d4b7d84
MrVictor2 <v.suarez6@gmail.com>
2024-03-24 00:12:34 -0400

    The correct one

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  const username = req.query.username;
+  console.log(`username: ${username}`);
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
